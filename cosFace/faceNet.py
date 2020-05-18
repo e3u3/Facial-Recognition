1ec351d588d665bc924d74f1517464e7a81a1552
jbk001 <guppykang@gmail.com>
2020-05-17 22:06:28 -0700

    q32

--- cosFace/faceNet.py
+++ cosFace/faceNet.py
@@ -62,8 +62,7 @@ class CustomLoss(nn.Module):
         accuracy = (predictedLabel.eq(target.data).cpu().sum().item() ) / float(target.size(0) )
 
         return loss, accuracy
-
-
+    
 class faceNet(nn.Module):
     def __init__(self,classnum=10574, feature=False, m = 1.35):
         super(faceNet, self).__init__()
@@ -144,6 +143,129 @@ class faceNet(nn.Module):
 
         x = self.relu4_1(self.conv4_1(x) )
         x = x + self.relu4_3(self.conv4_3(self.relu4_2(self.conv4_2(x) ) ) )
+
+        x = x.view(x.size(0),-1)
+        x = self.fc5(x)
+
+        if self.feature:
+            return x
+
+        x = self.fc6(x)
+        return x
+
+class faceNet_BN(nn.Module):
+    def __init__(self,classnum=10574, feature=False, m = 1.35):
+        super(faceNet, self).__init__()
+        self.classnum = classnum
+        self.feature = feature
+
+        # IMPLEMENT resdiual network 20-layer with batch normalization
+
+        self.conv1_1 = nn.Conv2d(3,64,3,2,1) #=>B*64*56*48
+        self.conv1_bn_1 = nn.BatchNorm2d(64)
+        self.relu1_1 = nn.PReLU(64)
+        
+        self.conv1_2 = nn.Conv2d(64,64,3,1,1)
+        self.conv1_bn_2 = nn.BatchNorm2d(64)
+        self.relu1_2 = nn.PReLU(64)
+        
+        self.conv1_3 = nn.Conv2d(64,64,3,1,1)
+        self.conv1_bn_3 = nn.BatchNorm2d(64)
+        self.relu1_3 = nn.PReLU(64)
+
+        self.conv2_1 = nn.Conv2d(64,128,3,2,1) #=>B*128*28*24
+        self.conv2_bn_1 = nn.BatchNorm2d(128)
+        self.relu2_1 = nn.PReLU(128)
+        
+        self.conv2_2 = nn.Conv2d(128,128,3,1,1)
+        self.conv2_bn_2 = nn.BatchNorm2d(128)
+        self.relu2_2 = nn.PReLU(128)
+        
+        self.conv2_3 = nn.Conv2d(128,128,3,1,1)
+        self.conv2_bn_3 = nn.BatchNorm2d(128)
+        self.relu2_3 = nn.PReLU(128)
+
+        self.conv2_4 = nn.Conv2d(128,128,3,1,1) #=>B*128*28*24
+        self.conv2_bn_4 = nn.BatchNorm2d(128)
+        self.relu2_4 = nn.PReLU(128)
+        
+        self.conv2_5 = nn.Conv2d(128,128,3,1,1)
+        self.conv2_bn_5 = nn.BatchNorm2d(128)
+        self.relu2_5 = nn.PReLU(128)
+
+        self.conv3_1 = nn.Conv2d(128,256,3,2,1) #=>B*256*14*12
+        self.conv3_bn_1 = nn.BatchNorm2d(256)
+        self.relu3_1 = nn.PReLU(256)
+        
+        self.conv3_2 = nn.Conv2d(256,256,3,1,1)
+        self.conv3_bn_2 = nn.BatchNorm2d(256)
+        self.relu3_2 = nn.PReLU(256)
+        
+        self.conv3_3 = nn.Conv2d(256,256,3,1,1)
+        self.conv3_bn_3 = nn.BatchNorm2d(256)
+        self.relu3_3 = nn.PReLU(256)
+
+        self.conv3_4 = nn.Conv2d(256,256,3,1,1) #=>B*256*14*12
+        self.conv3_bn_4 = nn.BatchNorm2d(256)
+        self.relu3_4 = nn.PReLU(256)
+        
+        self.conv3_5 = nn.Conv2d(256,256,3,1,1)
+        self.conv3_bn_5 = nn.BatchNorm2d(256)
+        self.relu3_5 = nn.PReLU(256)
+
+        self.conv3_6 = nn.Conv2d(256,256,3,1,1) #=>B*256*14*12
+        self.conv3_bn_6 = nn.BatchNorm2d(256)
+        self.relu3_6 = nn.PReLU(256)
+        
+        self.conv3_7 = nn.Conv2d(256,256,3,1,1)
+        self.conv3_bn_7 = nn.BatchNorm2d(256)
+        self.relu3_7 = nn.PReLU(256)
+
+        self.conv3_8 = nn.Conv2d(256,256,3,1,1) #=>B*256*14*12
+        self.conv3_bn_8 = nn.BatchNorm2d(256)
+        self.relu3_8 = nn.PReLU(256)
+        
+        self.conv3_9 = nn.Conv2d(256,256,3,1,1)
+        self.conv3_bn_9 = nn.BatchNorm2d(256)
+        self.relu3_9 = nn.PReLU(256)
+
+        self.conv4_1 = nn.Conv2d(256,512,3,2,1) #=>B*512*7*6
+        self.conv4_bn_1 = nn.BatchNorm2d(512)
+        self.relu4_1 = nn.PReLU(512)
+        
+        self.conv4_2 = nn.Conv2d(512,512,3,1,1)
+        self.conv4_bn_2 = nn.BatchNorm2d(512)
+        self.relu4_2 = nn.PReLU(512)
+        
+        self.conv4_3 = nn.Conv2d(512,512,3,1,1)
+        self.conv4_bn_3 = nn.BatchNorm2d(512)
+        self.relu4_3 = nn.PReLU(512)
+
+        self.fc5 = nn.Linear(512*7*6,512)
+        self.fc6 = CustomLinear(in_features = 512,
+                out_features = self.classnum, m=m)
+
+
+    def forward(self, x):
+        
+        # IMPLEMENT forward of network
+
+        x = self.relu1_1(self.conv1_bn_1(self.conv1_1(x)))
+        x = x + self.relu1_3(self.conv1_bn_3(self.conv1_3(self.relu1_2(self.conv1_bn_2(self.conv1_2(x))))))
+        
+
+        x = self.relu2_1(self.conv2_bn_1(self.conv2_1(x)))
+        x = x + self.relu2_3(self.conv2_bn_3(self.conv2_3(self.relu2_2(self.conv2_bn_2(self.conv2_2(x))))))
+        x = x + self.relu2_5(self.conv2_bn_5(self.conv2_5(self.relu2_4(self.conv2_bn_4(self.conv2_4(x))))))
+
+        x = self.relu3_1(self.conv3_bn_1(self.conv3_1(x) ))
+        x = x + self.relu3_3(self.conv3_bn_3(self.conv3_3(self.relu3_2(self.conv3_bn_2(self.conv3_2(x))))))
+        x = x + self.relu3_5(self.conv3_bn_5(self.conv3_5(self.relu3_4(self.conv3_bn_4(self.conv3_4(x))))))
+        x = x + self.relu3_7(self.conv3_bn_7(self.conv3_7(self.relu3_6(self.conv3_bn_6(self.conv3_6(x))))))
+        x = x + self.relu3_9(self.conv3_bn_9(self.conv3_9(self.relu3_8(self.conv3_bn_8(self.conv3_8(x))))))
+
+        x = self.relu4_1(self.conv4_bn_1(self.conv4_1(x) ))
+        x = x + self.relu4_3(self.conv4_bn_3(self.conv4_3(self.relu4_2(self.conv4_bn_2(self.conv4_2(x) ) )) ))
 
         x = x.view(x.size(0),-1)
         x = self.fc5(x)
